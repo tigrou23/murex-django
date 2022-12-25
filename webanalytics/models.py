cae6eb2d435540701c195d4d59bc238b4164700c
Hugo Pereira <54220880+tigrou23@users.noreply.github.com>
2022-12-25 10:37:50 +0100

    Revert "default integer etage"
    This reverts commit d1195f595a534ca0abe7f217f2f2e1f9671798da.

--- webanalytics/models.py
+++ webanalytics/models.py
@@ -23,7 +23,7 @@ STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
 class Booking(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     roomid = models.TextField()
-    etage = models.IntegerField(default = -1)
+    etage = models.IntegerField(-1)
     #date = models.TextField(default = "no date")
 
     class Meta:
