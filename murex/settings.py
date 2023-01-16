2531a1e706ef150de49806183663553f484f1f95
tigrou23 <hugo0807pereira@gmail.Com>
2023-01-16 16:21:49 +0000

    allowed

--- murex/settings.py
+++ murex/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.corsmiddleware"
+    "corsheaders.middleware.CorsMiddleware"
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
